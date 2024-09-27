a6afd7362acab3e9f901644462813151463534cc
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2024-09-26 21:56:03 -0700

    updated gradio_ui

--- gradio_ui.py
+++ gradio_ui.py
@@ -128,8 +128,8 @@ def run_gradio():
 
 if __name__ == "__main__":
     # Start Flask in a separate process
-    # flask_process = multiprocessing.Process(target=run_flask)
-    # flask_process.start()
+    flask_process = multiprocessing.Process(target=run_flask)
+    flask_process.start()
 
     # Run Gradio for the chat interface
     run_gradio()
