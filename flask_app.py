0ec710e43d9faeb71b4db3ce461eaff94404acc1
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2024-09-27 08:49:56 -0700

    updated flask app

--- flask_app.py
+++ flask_app.py
@@ -6,6 +6,19 @@ from gradio_ui import create_gradio_interface
 app = Flask(__name__)
 CORS(app, resources={r"/token": {"origins": ["https://chat-w3innovation.pythonanywhere.com", "https://w3Innovation.pythonanywhere.com"]}})
 
+# Serve Gradio interface on the root route
+@app.route("/", methods=["GET"])
+def run_gradio():
+    demo = create_gradio_interface()
+    return demo.launch(
+        share=False, 
+        inline=True, 
+        prevent_thread_lock=True, 
+        server_name="127.0.0.1",
+        # show_error=False,
+        # show_api=False
+    )  # Prevent thread lock
+
 
 # Route to receive token
 @app.route('/token', methods=['POST'])
@@ -31,21 +44,6 @@ def receive_token():
         return redirect(url_for('login'))  # Use `url_for` for better URL management
 
     return jsonify({'status': 'Success', 'message': 'Token received'})
-
-
-# Serve Gradio interface on the root route
-@app.route("/", methods=["GET", "POST"])
-def run_gradio():
-    demo = create_gradio_interface()
-    return demo.launch(
-        share=False, 
-        inline=True, 
-        prevent_thread_lock=True, 
-        server_name="127.0.0.1",
-        # show_error=False,
-        # show_api=False
-    )  # Prevent thread lock
-
 
 if __name__ == "__main__":
     app.run()
